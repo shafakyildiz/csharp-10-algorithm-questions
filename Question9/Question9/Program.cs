091f2c62db2e81f3247500fa2b2ffd12468b46c0
Şafak Yıldız <shafakyildiz@gmail.com>
2022-02-05 22:50:17 +0300

    Add files via upload

---
+++ Question9/Question9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Question9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bir dizinin 2.büyük elemanını bulmak
+            // INPUT => 1,4,2,-1,18,221,44,123,1,33,-10
+            // OUTPUT => 123
+            int[] array = { 1, 4, 2, -1, 18, 221, 44, 123, 1, 33, -10 };
+            Array.Sort(array); // Sorting array  
+            Array.Reverse(array); // Reverse Sorting array value  
+            Console.WriteLine(array[1]);
+        }
+    }
+}
