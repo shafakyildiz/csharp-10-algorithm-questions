4d88b63dc974be6c04441f88eb4c238cd2190c04
Şafak Yıldız <shafakyildiz@gmail.com>
2022-02-05 21:57:15 +0300

    Add files via upload

---
+++ Question3/Question3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list1 = { 13, 2, 10, 4, 0, 3, 12, 44, 112 };
+            double minimum = list1.Min();
+            int minIndex = Array.IndexOf(list1, list1.Min()) + 1;
+            Console.WriteLine("Dizinin en küçük elemanı {0} dır. Bulunduğu yer {1} Sıradır.", minimum, minIndex);
+        }
+    }
+}
