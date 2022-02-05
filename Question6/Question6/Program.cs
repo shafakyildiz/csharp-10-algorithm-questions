a37038f7b1500622fbae234c12abaecbed5e8256
Şafak Yıldız <shafakyildiz@gmail.com>
2022-02-05 22:59:20 +0300

    Add files via upload

---
+++ Question6/Question6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Question6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0 - 100000 arasındaki tam kare sayıları ekrana yazdırınız.
+            // OUTPUT => 2,3,4,5 …. (x’2 < 10000) olana kadar devam eder
+            var x = 2;
+            while (x*x < 10000)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        }
+    }
+}
