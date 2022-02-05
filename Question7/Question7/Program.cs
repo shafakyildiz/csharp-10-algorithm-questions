2173b9a432aca84295863e979b56a412abe11f43
Şafak Yıldız <shafakyildiz@gmail.com>
2022-02-05 22:58:26 +0300

    Add files via upload

---
+++ Question7/Question7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Question7
+{
+    class Program
+    {
+        // Fibonacci serisinin ilk 10 sayısının ekrana basımını yazınız
+        // OUTPUT: 0 1 1 2 3 5 8 13 21 34
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write("{0} {1}", a, b);
+            for (int i = 2; i < 10; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
