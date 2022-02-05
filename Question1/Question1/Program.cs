d2a27045473737b3b5053749ce41adda5b47aa0e
Şafak Yıldız <shafakyildiz@gmail.com>
2022-02-05 21:48:00 +0300

    Add files via upload

---
+++ Question1/Question1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tempSymbol = "K";
+            var tempDeg = 273;
+            Console.WriteLine("Your temp is: {0} C and {1} K", tempDeg - 273, (tempDeg - 273.15) * 1.8 + 32 );
+        }
+
+    }
+}
---
+++ Question2/Question2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string repeatedWord = "Bir saat sonra bütün görevlerimi JIRA üzerinden 'successfully' olarak çevireceğim";
+            for (int i = 0; i < repeatedWord.Count() - 1; i++)
+            {
+                if (repeatedWord[i] == repeatedWord[i + 1])
+                {
+                    var result = string.Concat(repeatedWord[i], repeatedWord[i + 1]);
+                    Console.WriteLine(result);
+
+                }
+            }
+        }
+    }
+}
