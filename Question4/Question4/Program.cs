edea3b56d3e45f01b0d256bccc145d276637a8e6
Şafak Yıldız <shafakyildiz@gmail.com>
2022-02-06 13:08:32 +0300

    Add files via upload

---
+++ Question4/Question4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // INPUT => 123
+            // OUTPUT => 1,2,3 – 12,13,21,23,31,32 – 123
+
+            var num = Convert.ToString(123);
+            Console.WriteLine(num);
+            var i = 0;
+            num = string.Concat(num, num);
+            while (i < num.Length - 1)
+            {
+                Console.WriteLine("{0}{1}", num[i], num[i+1]);
+                if (i < num.Length - 2)
+                {
+                    Console.WriteLine("{0}{1}", num[i], num[i + 2]);
+                }
+                i++;
+            }
+        }
+    }
+}
