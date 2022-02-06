7f0a3bd8c148bdc1ae0ac16044b1bb6a706e40ba
Şafak Yıldız <shafakyildiz@gmail.com>
2022-02-06 20:05:58 +0300

    Add files via upload

---
+++ Question8/Question8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class PrimeNumberExample
+{
+    public static void Main(string[] args)
+    {
+        int sumOfNotPrimes = 0;
+        for (int n = 2; n < 100; n++)
+        {
+            int i, m = 0, flag = 0;
+            m = n / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("{0} is not Prime.", n);
+                    flag = 1;
+                    sumOfNotPrimes += n;
+                    break;
+                }
+            }
+            Console.WriteLine("Sum of not prime numbers: {0}", sumOfNotPrimes);
+
+            if (flag == 0)
+                Console.WriteLine("{0} is Prime.", n);
+        }
+    
+    }
+}
