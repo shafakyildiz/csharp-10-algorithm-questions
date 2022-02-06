9110294ba9394716d5b17da603e4199dca3f36cd
Şafak Yıldız <shafakyildiz@gmail.com>
2022-02-06 19:56:14 +0300

    Add files via upload

---
+++ Question5/Question5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find duplicate words in a string...
+            // asla = 2
+            // hiç = 2
+            // bir = 2
+            // zaman = 2
+            var myStr = "Asla hiç bir zaman asla deme ki ; hiç bir zaman nolacağı belli olmaz";
+            myStr = myStr.ToLower();
+            var splitted = myStr.Split(' ');
+            int count;
+            for (int i = 0; i < splitted.Length - 1; i++)
+            {
+                count = 1;
+                for (int j = i+1; j < splitted.Length; j++)
+                {
+                    if (splitted[i] == splitted[j])
+                    {
+                        count++;
+                        splitted[j] = "0";
+                    }
+                }
+                if (count > 1 && splitted[i] != "0")
+                {
+                    Console.WriteLine("{0} = {1}", splitted[i], count);
+                }
+            }
+         
+        }
+    }
+}
